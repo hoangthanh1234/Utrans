15f2791a0b9e0cac6511810cbaf9b976e0ecd297
thanhtw <thanhtctv6@gmail.com>
2024-04-05 02:45:49 +0000

    update encoder

--- models/utrans_encoder.py
+++ models/utrans_encoder.py
@@ -32,10 +32,10 @@ class Utrans_encoder(nn.Module):
 
         # Build encoder         
         
-        self.node_1 = ResContextBlock(in_channels,base_channels,)#32
-        self.node_2 = ResContextBlock(base_channels,base_channels*2)#64
-        self.node_3 = ResContextBlock(base_channels*2,base_channels*4)#128
-        self.node_4 = ResContextBlock(base_channels*4,base_channels*8)#256
+        self.node_1 = Encoder_node(in_channels,base_channels,)#32
+        self.node_2 = Encoder_node(base_channels,base_channels*2)#64
+        self.node_3 = Encoder_node(base_channels*2,base_channels*4)#128
+        self.node_4 = Encoder_node(base_channels*4,base_channels*8)#256
 
 
         assert patch_stride[0] % 2 == 0
@@ -57,10 +57,30 @@ class Utrans_encoder(nn.Module):
         self.flatten = flatten
         
         resize_in = [64,160,352,704] #the inputsize for next block in encoder
-        self.resize_channel_1 = nn.Conv2d(resize_in[0],base_channels, kernel_size=(1, 1), stride=1)
-        self.resize_channel_2 = nn.Conv2d(resize_in[1],base_channels*2, kernel_size=(1, 1), stride=1)
-        self.resize_channel_3 = nn.Conv2d(resize_in[2],base_channels*4, kernel_size=(1, 1), stride=1)
-        self.resize_channel_4 = nn.Conv2d(resize_in[3],base_channels*8, kernel_size=(1, 1), stride=1)
+
+        self.resize_channel_1 = nn.Sequential(
+            nn.Conv2d(resize_in[0],base_channels, kernel_size=(1, 1), stride=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(base_channels)
+        )
+
+        self.resize_channel_2 = nn.Sequential(
+            nn.Conv2d(resize_in[1],base_channels, kernel_size=(1, 1), stride=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(base_channels*2)
+        )
+
+        self.resize_channel_3 = nn.Sequential(
+            nn.Conv2d(resize_in[2],base_channels, kernel_size=(1, 1), stride=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(base_channels*4)
+        )
+
+        self.resize_channel_1 = nn.Sequential(
+            nn.Conv2d(resize_in[3],base_channels, kernel_size=(1, 1), stride=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(base_channels*8)
+        )      
        
 
     def get_grid_size(self, H, W):
@@ -70,7 +90,6 @@ class Utrans_encoder(nn.Module):
         B, C, H, W = x.shape  # B, in_channels, image_size[0], image_size[1]   
 
         shortcut1, node_1 = self.node_1(x,previous=None,Node=1) 
-       
         reChannel_1 = self.resize_channel_1(node_1) 
         
         shortcut2, node_2 = self.node_2(reChannel_1,previous=None,Node =2)
@@ -101,51 +120,87 @@ class Utrans_encoder(nn.Module):
          }
        
        
-        #print(f"shortcut1 {shortcut1.shape},shortcut2: {shortcut2.shape}, shortcut3: {shortcut3.shape}, shortcut4: {shortcut4.shape}, reChannel_1: {reChannel_1.shape}, reChannel_2: {reChannel_2.shape},reChannel_3: {reChannel_3.shape}, reChannel_4: {reChannel_4.shape}, x_proj: {x_proj.shape}, x_latten: {x_latten.shape}")
+        print(f"shortcut1 {shortcut1.shape},shortcut2: {shortcut2.shape}, shortcut3: {shortcut3.shape}, shortcut4: {shortcut4.shape}, reChannel_1: {reChannel_1.shape}, reChannel_2: {reChannel_2.shape},reChannel_3: {reChannel_3.shape}, reChannel_4: {reChannel_4.shape}, x_proj: {x_proj.shape}, x_latten: {x_latten.shape}")
         
         return results 
 
-class ResContextBlock(nn.Module):
+class Encoder_node(nn.Module):
 
     def __init__(self, in_filters, out_filters,dropout_rate=0.1,stride=1,previous=None):
-        super(ResContextBlock, self).__init__()       
+        super(Encoder_node, self).__init__()   
+
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)      
        
         self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=stride)
         self.act1 = nn.LeakyReLU()
-
-        self.conv2 = nn.Conv2d(out_filters, out_filters, kernel_size=(3, 3), padding=1)        
-        self.act2 = nn.LeakyReLU()              
         self.bn1 = nn.BatchNorm2d(out_filters)
+
+        self.conv2 = nn.Conv2d(out_filters, out_filters, kernel_size=(2, 2), padding=1)        
+        self.act2 = nn.LeakyReLU()              
+        self.bn2 = nn.BatchNorm2d(out_filters)
+
+
 
         self.conv3 = nn.Conv2d(out_filters, out_filters, kernel_size=(3, 3), dilation=2, padding=2)
         self.act3 = nn.LeakyReLU()       
-        self.bn2 = nn.BatchNorm2d(out_filters)
+        self.bn3 = nn.BatchNorm2d(out_filters)
+
+        self.conv4 = nn.Conv2d(out_filters, out_filters, kernel_size=(3, 3), dilation=2, padding=2)
+        self.act4 = nn.LeakyReLU()       
+        self.bn4 = nn.BatchNorm2d(out_filters)
+
+        self.conv5 = nn.Conv2d(out_filters, out_filters, kernel_size=(2, 2), dilation=2, padding=1)
+        self.act5 = nn.LeakyReLU()       
+        self.bn5 = nn.BatchNorm2d(out_filters)
+
+        self.conv6 = nn.Conv2d(out_filters, out_filters, kernel_size=(1, 1), stride=stride)
+        self.act6 = nn.LeakyReLU()
+        self.bn6 = nn.BatchNorm2d(out_filters)
+
        
         self.dropout = nn.Dropout2d(p=dropout_rate)
-        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)            
+                  
 
     def forward(self, x, previous,Node):
         shortcut = self.conv1(x)
         shortcut = self.act1(shortcut) 
+        shortcut = self.bn1(shortcut)
+
+        shortcut = self.conv2(shortcut)
+        shortcut = self.act2(shortcut)
+        shortcut = self.bn2(shortcut)
         shortcut_pool = self.pool(shortcut)
 
-        resA = self.conv2(shortcut)
-        resA = self.act2(resA)        
-        resA = self.bn1(resA) 
 
-        resA1 = self.conv3(resA)
-        resA1 = self.act3(resA1)
-        resA1 = self.bn2(resA1)
-        resA1_pool = self.pool(resA1)
+        resA1 = self.conv3(shortcut_pool)
+        resA1 = self.act3(resA)        
+        resA1 = self.bn3(resA) 
+
+        resA2 = self.conv4(resA1)
+        resA2 = self.act4(resA2)
+        resA2 = self.bn4(resA2)
+
+        resA3 = self.conv5(resA2)
+        resA3 = self.act5(resA3)
+        resA3 = self.bn5(resA3)
+
+        resA3_plus = shortcut_pool + resA3
+
+        resA4 = self.conv6(resA3_plus)
+        resA4 = self.act6(resA4)
+        resA4 = self.bn6(resA4)       
         
 
         if  previous != None:        #cat is concatenation
-            output = torch.cat((resA1_pool,shortcut_pool,self.pool(x),self.pool(self.pool(previous))),dim=1)
+            output = torch.cat((resA1,resA2,resA3,resA4,self.pool(x),self.pool(self.pool(previous))),dim=1)
         else:
             if Node == 2:
-                output = torch.cat((resA1_pool,shortcut_pool,self.pool(x)),dim=1)
+                output = torch.cat((resA1, resA2, resA3, resA4, self.pool(x)),dim=1)
             else:
-                output = torch.cat((resA1_pool,shortcut_pool),dim=1)
+                output = torch.cat((resA1, resA2, resA3, resA4),dim=1)
                
-        output = self.dropout(output)       
+        output = self.dropout(output)  
+
+        print("OutPut_shape: ", output.shape)
+
         return shortcut_pool, output, 
